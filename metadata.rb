ad604ac1952838058805587a82efaccb2ff4838f
Matthew Thode <mthode@mthode.org>
2014-03-12 12:11:06 -0500

    adding what we support

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,8 @@ license          'Apache 2.0'
 description      'Installs/Configures rackspace_iptables'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.2.1'
+
+supports 'centos'
+supports 'debian'
+supports 'rhel'
+supports 'ubuntu'
