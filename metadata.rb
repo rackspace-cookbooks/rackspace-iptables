03d80ad1d509b6b6975c1dff1cdf7d5e7e87ac7e
Matthew Thode <matt.thode@rackspace.com>
2014-03-20 19:56:12 +0000

    initial broken search fun

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rackspace-cookbooks@rackspace.com'
 license          'Apache 2.0'
 description      'Installs/Configures rackspace_iptables'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.1'
+version          '1.3.0'
 
 supports 'centos'
 supports 'debian'
