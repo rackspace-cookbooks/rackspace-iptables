09b4e2a12592d77d55c87db7f051284dda0e6a7a
Martin Smith <martin@mbs3.org>
2014-12-12 14:22:11 -0600

    Bump version for dev

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rackspace-cookbooks@rackspace.com'
 license          'Apache 2.0'
 description      'Installs/Configures rackspace_iptables'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.7.2'
+version          '1.7.3'
 
 supports 'centos'
 supports 'debian'
